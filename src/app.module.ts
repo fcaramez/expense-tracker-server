b094be5aad0bda99049c1cefc859ada100151b45
fcaramez <francisco.caramez.duarte@gmail.com>
2023-06-18 18:12:57 +0100

    adds expense controller, service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ExpenseModule } from './expense/expense.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { PrismaModule } from './prisma/prisma.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ExpenseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
